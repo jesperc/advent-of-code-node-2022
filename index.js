396e8a43bb86f236dedeba2537358685c596616b
Jesper Corell <jesper.corell@acnestudios.com>
2022-12-03 13:40:57 +0100

    added execution times for solutions

--- index.js
+++ index.js
@@ -6,11 +6,15 @@ const main = async () => {
     const day = i + 1;
     if (day <= lastCompletedDay) {
       await new Promise((resolve) => {
+        console.time("time");
         exec(`node ./${day}/part${1}.js`, (error, stdout, stderr) => {
           console.log(`# --- December ${day} 2022 --- #`);
+          console.timeEnd("time");
           console.log(`part 1: ${stdout}`);
 
+          console.time("time");
           exec(`node ./${day}/part${2}.js`, (error, stdout, stderr) => {
+            console.timeEnd("time");
             console.log(`part 2: ${stdout}`);
             resolve();
           });
